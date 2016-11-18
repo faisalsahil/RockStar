409a116bd15df19514f3f14cdd3038598aa2f927
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2016-11-19 01:59:14 +0500

    n

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   
   def login
-    params = {
-        user: {
-            email: 'faisal@gmail.com',
-            password: 'zaqxsw1234'
-        }
-    }
+    # params = {
+    #     user: {
+    #         email: 'faisal@gmail.com',
+    #         password: 'zaqxsw1234'
+    #     }
+    # }
     
     resp_data = UserSession.login(params)
     return render json: resp_data
