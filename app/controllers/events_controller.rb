6ec3a7a7e49cbf4825dc5ec15dd88d1b239f4323
Faisal Bhatti <faisalbhatti@appsgenii.eu>
2016-11-24 15:08:37 +0500

    n

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   
   def index
-    @active_events    = Event.where('end_date > ?',   Date.today)
+    @active_events    = Event.where('start_date < ? AND end_date > ?', Date.today,  Date.today)
     @pending_events   = Event.where('start_date > ?', Date.today)
     @completed_events = Event.where('end_date < ?',   Date.today)
   end
